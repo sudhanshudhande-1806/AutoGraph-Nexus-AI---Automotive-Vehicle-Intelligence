429bd890cbbf114d969c2c7c7f15684afd3772f4
Sudhanshu Dhande <sudhanshudhande@gmail.com>
2026-06-02 02:57:54 +0530

    Module 2 Kafka Streaming Platform

--- kafka/producer/telemetry_producer.py
+++ kafka/producer/telemetry_producer.py
@@ -1,49 +1,21 @@
-from kafka import KafkaProducer
-import json
-import time
 import os
 import sys
+import json
+import time
 
-sys.path.append(
-    os.path.abspath(
-        os.path.join(
-            os.path.dirname(__file__),
-            "../../simulator"
-        )
+PROJECT_ROOT = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        "../.."
     )
 )
 
-from vehicle_generator import generate_vehicle
-from gps_generator import generate_gps
-from fault_generator import generate_fault
-from weather_generator import generate_weather
-from maintenance_generator import generate_maintenance
+sys.path.insert(0, PROJECT_ROOT)
 
-producer = KafkaProducer(
-    bootstrap_servers="localhost:9092",
-    value_serializer=lambda v:
-        json.dumps(v).encode("utf-8")
-)
+from simulator.vehicle_generator import generate_vehicle
+from simulator.gps_generator import generate_gps
+from simulator.fault_generator import generate_fault
+from simulator.weather_generator import generate_weather
+from simulator.maintenance_generator import generate_maintenance
 
-print("Producer Started...")
-
-while True:
-
-    event = {
-
-        **generate_vehicle(),
-        **generate_gps(),
-        **generate_fault(),
-        **generate_weather(),
-        **generate_maintenance()
-
-    }
-
-    producer.send(
-        "vehicle-telemetry",
-        event
-    )
-
-    print(event)
-
-    time.sleep(1)
+from kafka import KafkaProducer
